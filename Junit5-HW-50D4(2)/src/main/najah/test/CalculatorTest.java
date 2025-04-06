48dd0d1014d6ccf9bc302e797f9208aab5cdfc61
SuzanAqraa <152098398+SuzanAqraa@users.noreply.github.com>
2025-04-06 23:10:15 +0200

    Update CalculatorTest.java

--- Junit5-HW-50D4(2)/src/main/najah/test/CalculatorTest.java
+++ Junit5-HW-50D4(2)/src/main/najah/test/CalculatorTest.java
@@ -103,13 +103,4 @@ public class CalculatorTest {
         assertEquals(120, calculator.factorial(5));
     }
 
-    @Test
-  //  @Disabled
-    @Order(8)
-    @DisplayName("Forced failure test")
-    void intentionalFailingTest() {
-        assertEquals(15, calculator.add(10, 5)); // Intentionally incorrect for failure
-        //correct value ->20 
-        // to make the avarege up 85% I but it 15 
-    }
 }
